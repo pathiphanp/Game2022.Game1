7debdafab29bab2cdc27a4f3c2a5abebc188a31e
pathiphanp <99470192+pathiphanp@users.noreply.github.com>
2022-07-26 00:57:34 +0700

    02

--- Shooting_2D_game/Assets/Script/Ship_normal.cs
+++ Shooting_2D_game/Assets/Script/Ship_normal.cs
@@ -7,12 +7,17 @@ public class Ship_normal : Healthy_ship
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void enemy_destroy(float damageAmount)
+    {
+        base.enemy_destroy(damageAmount);
     }
 }
--- Shooting_2D_game/Assets/Script/Ship_roll.cs
+++ Shooting_2D_game/Assets/Script/Ship_roll.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship_roll : MonoBehaviour
+public class Ship_roll : Healthy_ship
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void enemy_destroy(float damageAmount)
+    {
+        base.enemy_destroy(damageAmount);
     }
 }
