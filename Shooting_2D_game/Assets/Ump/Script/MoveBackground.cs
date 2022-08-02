fb57b073a623e980687f2fbddef9e64ccd474118
pathiphanp <99470192+pathiphanp@users.noreply.github.com>
2022-08-02 09:59:00 +0700

    02

--- Shooting_2D_game/Assets/Ump/Script/MoveBackground.cs
+++ Shooting_2D_game/Assets/Ump/Script/MoveBackground.cs
@@ -6,9 +6,11 @@ public class MoveBackground : MonoBehaviour
 {
     float length;
     float start_pos;
+    
     GameObject cam;
     [SerializeField] float parallax_effect;
-
+    [SerializeField] int time;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +23,21 @@ public class MoveBackground : MonoBehaviour
     void Update()
     {
         float temp = (cam.transform.position.x * (1 - parallax_effect));
-
         float distance = (cam.transform.position.x * parallax_effect);
-        transform.position = new Vector3(start_pos + distance,transform.position.y,transform.position.z);
+        float str_pus_dis = start_pos + distance;
+        transform.position = new Vector3( Mathf.Lerp(transform.position.x,str_pus_dis,time / Time.deltaTime),transform.position.y,transform.position.z);
 
         if (temp > start_pos + length)
         {
-            start_pos += length;
+            float gopoint = start_pos += length;
+            Mathf.Lerp(start_pos,gopoint,1 * Time.deltaTime);
+
         }
 
         else if (temp < start_pos - length)
         {
-            start_pos -= length;
+            float backpoint = start_pos -= length;
+            Mathf.Lerp(start_pos,backpoint,1 * Time.deltaTime);
         }
     }
 }
