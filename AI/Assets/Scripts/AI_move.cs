54e27de6f9109a367a48aed1b0d906ebae4ee03d
pathiphanp <99470192+pathiphanp@users.noreply.github.com>
2022-08-04 02:11:18 +0700

    02

--- AI/Assets/Scripts/AI_move.cs
+++ AI/Assets/Scripts/AI_move.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class AI_move : MonoBehaviour
 {
+    public GameObject target;
+
     Rigidbody rb;
     [SerializeField] bool str_rnd;
 
-    int i;
+    int i = 0   ;
     [SerializeField] float delay;
+    [SerializeField] float delay_normal;
+    [SerializeField] float delay_atk;
     [SerializeField] float speed_ro;
+    [SerializeField] float speed_ro_reverse;
     [SerializeField] float speed_move;
+    [SerializeField] float speed_move_atk;
 
     private void Awake()
     {
@@ -33,26 +39,30 @@ public class AI_move : MonoBehaviour
         System.Random rnd = new System.Random();
         if (str_rnd == true)
         {
-           i = rnd.Next(0, 2);
+            speed_ro_reverse = 0;
+            i = rnd.Next(0, 4);
             str_rnd = false;
             StartCoroutine(delay_rnd());         
         }
-
-
         if (i == 0)
         {
             rb.velocity = transform.forward * speed_move;
-            transform.Rotate(0, speed_ro * Time.deltaTime, 0);
+            transform.Rotate(0, (speed_ro * Time.deltaTime), 0);
         }
-
         else if (i == 1)
         {
             rb.velocity = transform.forward * speed_move;
-            transform.Rotate(0, -speed_ro * Time.deltaTime, 0);
+            transform.Rotate(0, (-speed_ro * Time.deltaTime), 0);
         }
         else if (i == 2)
         {
             rb.velocity = transform.forward * speed_move;
+        }
+        else if (i == 3)
+        {
+            transform.LookAt(target.transform.position);
+            transform.Translate(rb.velocity = transform.forward * speed_move * Time.deltaTime);
+
         }
     }
 
@@ -61,4 +71,29 @@ public class AI_move : MonoBehaviour
         yield return new WaitForSeconds(delay);
         str_rnd = true;
     }
+
+    private void OnCollisionEnter(Collision ai)
+    {
+        if (ai.gameObject.tag == "wallleft" )
+        {
+           speed_ro_reverse = -320;
+           transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + speed_ro_reverse, transform.rotation.z);
+        }
+        else if (ai.gameObject.tag == "wallrigth")
+        {
+            speed_ro_reverse = -120;
+            transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + speed_ro_reverse, transform.rotation.z);
+        }
+        else if (ai.gameObject.tag == "walldown")
+        {
+            speed_ro_reverse = 320;
+            transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + speed_ro_reverse, transform.rotation.z);
+        }
+        if (ai.gameObject.tag == "walltop")
+        {
+            speed_ro_reverse = 120;
+            transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + speed_ro_reverse, transform.rotation.z);
+        }
+    }
+
 }
